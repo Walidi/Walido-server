87e68d1d1ba07766fecacb8010088b1683496c64
Walidi <waab18ab@student.cbs.dk>
2022-03-25 00:04:51 +0100

    httpOnly: false

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ app.use(
     saveUninitialized: false,
     cookie: {  //How long will the cookie live for?
       expires: 60 * 60 * 1000, //Expires after one hour
-      secure: true
+      secure: true,
+      httpOnly: false
     }
   }));
 
