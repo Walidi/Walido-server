153e3c3be582c389a1ae8b36fdb60bccf4006b7d
Walidi <waab18ab@student.cbs.dk>
2022-09-28 14:59:02 +0200

    ....

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ app.use(cors({   //Parsing origin of the front-end
    credentials: true   //Allows cookies to be enabled
 }));  
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.get('/', (req, res) => res.send("Hi!"));
 
 app.use(cookieParser());
