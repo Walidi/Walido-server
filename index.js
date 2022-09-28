7dc0abdb7e9274af6d75607a864bb1282def57b5
Walidi <waab18ab@student.cbs.dk>
2022-09-28 15:06:23 +0200

    changes......

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cors({   //Parsing origin of the front-end
 }));  
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
