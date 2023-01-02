b974f3a48b3151dbdcc61f441eda474a50fe1646
Walidi <waab18ab@student.cbs.dk>
2023-01-02 17:43:24 +0100

    trying without using firebase file

--- index.js
+++ index.js
@@ -15,9 +15,28 @@ const bcrypt = require('bcryptjs'); //Cryption function
 const saltRounds = 10;
 const jwt = require('jsonwebtoken');
 //const { response } = require("express");
-const storage = require('./firebase');
+
+//const storage = require('./firebase');
 const firebaseRef = require('firebase/storage');
 const firebaseUpload = require('firebase/storage');
+const firebaseApp = require("firebase/app");
+const firebaseStorage = require('firebase/storage');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAw80HvGbV0yrmrHJYWiivc3f1912YcIF4",
+  authDomain: "webauth-2ac45.firebaseapp.com",
+  projectId: "webauth-2ac45",
+  storageBucket: "webauth-2ac45.appspot.com",
+  messagingSenderId: "136626980163",
+  appId: "1:136626980163:web:963db3dbdaf09d9bab5bf6",
+  measurementId: "G-W2THV32VWK"
+};
+
+// Initialize Firebase
+const fApp = firebaseApp.initializeApp(firebaseConfig);
+
+const storage = firebaseStorage.getStorage(fApp);
+
 const v4 = require('uuid');
 
 const app = express();
