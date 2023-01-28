34ba8d7a702e8163913a0d7878ee986518b27bf5
Walidi <waab18ab@student.cbs.dk>
2023-01-28 18:10:55 +0100

    seeing if we show filename when file is displayed

--- index.js
+++ index.js
@@ -160,11 +160,11 @@ app.get('/getCV', verifyJWT, async(req, res, next) => {
         if (result.length>0) { //Checking if query returns a row
         const fileName = result[0].name;
         const docID = result[0].docID;
-        const streamedFile = fs.createWriteStream(fileName);
         const storageRef = ref(storage, `cv_uploads/${docID}`);
 
         getDownloadURL(storageRef).then((url) => {
            https.get(url, function(file) {
+            res.header(`filename=${fileName}`);
             file.pipe(res);
           });
          })
